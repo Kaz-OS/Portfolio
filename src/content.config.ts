3d675a16b5d5b42a65a474c4c7298c3fbea265f6
Kaz-OS <estebanrattier27@gmail.com>
2025-04-21 16:27:48 +0200

    Up

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,8 +4,8 @@ import { glob } from 'astro/loaders';
 const veille = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/content/veille" }),
   schema: z.object({
-    title: z.string().min(26).max(100),
-    author: z.string().min(3).max(56),
+    title: z.string(),
+    author: z.string(),
     source: z.string().url(),
     boundaries: z.array(z.string().min(5).max(26)),
     date: z.coerce.date(),
